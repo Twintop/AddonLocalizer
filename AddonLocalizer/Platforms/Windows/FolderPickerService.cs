d0f9f9f6ecca6762f6acb43c3a86c86cb34038ca
David C. Uhrig <david@twintop-tahoe.com>
2026-01-22 01:53:25 +1100

    Fix some issues with the folder picker

--- AddonLocalizer/Platforms/Windows/FolderPickerService.cs
+++ AddonLocalizer/Platforms/Windows/FolderPickerService.cs
@@ -1,23 +1,88 @@
-using Windows.Storage.Pickers;
+using System.Runtime.InteropServices;
 
 namespace AddonLocalizer.Platforms.Windows;
 
 public class FolderPickerService : IFolderPickerService
 {
-    public async Task<string?> PickFolderAsync()
+    public Task<string?> PickFolderAsync()
     {
-        var folderPicker = new FolderPicker
+        return MainThread.InvokeOnMainThreadAsync(() =>
         {
-            SuggestedStartLocation = PickerLocationId.DocumentsLibrary,
-            ViewMode = PickerViewMode.List
-        };
-        
-        folderPicker.FileTypeFilter.Add("*");
+            var dialog = new FileOpenDialog() as IFileOpenDialog;
+            if (dialog is null)
+            {
+                return null;
+            }
 
-        var hwnd = ((MauiWinUIWindow)Application.Current?.Windows[0].Handler.PlatformView!).WindowHandle;
-        WinRT.Interop.InitializeWithWindow.Initialize(folderPicker, hwnd);
+            try
+            {
+                dialog.SetOptions(FOS.FOS_PICKFOLDERS | FOS.FOS_FORCEFILESYSTEM);
 
-        var folder = await folderPicker.PickSingleFolderAsync();
-        return folder?.Path;
+                var hr = dialog.Show(IntPtr.Zero);
+                if (hr < 0)
+                {
+                    return null; // User cancelled or error
+                }
+
+                dialog.GetResult(out var item);
+                item.GetDisplayName(SIGDN.SIGDN_FILESYSPATH, out var path);
+                return path;
+            }
+            finally
+            {
+                Marshal.ReleaseComObject(dialog);
+            }
+        });
+    }
+
+    [ComImport]
+    [Guid("DC1C5A9C-E88A-4dde-A5A1-60F82A20AEF7")]
+    private class FileOpenDialog { }
+
+    [ComImport]
+    [Guid("42f85136-db7e-439c-85f1-e4075d135fc8")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    private interface IFileOpenDialog
+    {
+        [PreserveSig] int Show(IntPtr parent);
+        void SetFileTypes();
+        void SetFileTypeIndex();
+        void GetFileTypeIndex();
+        void Advise();
+        void Unadvise();
+        void SetOptions(FOS fos);
+        void GetOptions();
+        void SetDefaultFolder();
+        void SetFolder();
+        void GetFolder();
+        void GetCurrentSelection();
+        void SetFileName();
+        void GetFileName();
+        void SetTitle();
+        void SetOkButtonLabel();
+        void SetFileNameLabel();
+        void GetResult(out IShellItem ppsi);
+    }
+
+    [ComImport]
+    [Guid("43826d1e-e718-42ee-bc55-a1e261c37bfe")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    private interface IShellItem
+    {
+        void BindToHandler();
+        void GetParent();
+        void GetDisplayName(SIGDN sigdnName, [MarshalAs(UnmanagedType.LPWStr)] out string ppszName);
+    }
+
+    [Flags]
+    private enum FOS : uint
+    {
+        FOS_PICKFOLDERS = 0x20,
+        FOS_FORCEFILESYSTEM = 0x40
+    }
+
+    private enum SIGDN : uint
+    {
+        SIGDN_FILESYSPATH = 0x80058000
     }
 }
